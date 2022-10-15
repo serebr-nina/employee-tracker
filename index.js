6330a965a054ac5483f48fa2a370e69fad73a265
NinaSerebryakova <serebr.nina@gmail.com>
2022-10-15 14:35:01 -0400

    made tables viewable and joined tables in 'viewEmployees' async function

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ const mainMenu = async () => {
                 {
                     name: 'View all departments',
                     value: 'VIEW_DEPARTMENTS'
+                },
+                {
+                    name: 'View all roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
+                    name: 'Exit',
+                    value: 'EXIT'
                 }
             ]
         }
@@ -38,6 +46,9 @@ const mainMenu = async () => {
         case 'VIEW_DEPARTMENTS':
             viewDepartments();
             break;
+        case 'VIEW_ROLES':
+            viewRoles();
+            break;
         case 'EXIT':
             process.exit();
         default:
@@ -46,14 +57,26 @@ const mainMenu = async () => {
 };
 
 const viewEmployees = async () => {
-    const [employeeData] = await db.query('SELECT * FROM employee');
-    cTable(employeeData);
+    const [employeeData] = await db.query(`SELECT employee.id, employee.first_name, employee.last_name, 
+                                                  role.title, role.salary, department.name AS departmnent,
+                                                  CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+                                            FROM employee 
+                                            LEFT JOIN role ON employee.role_id = role.id
+                                            LEFT JOIN department ON role.department_id = department.id
+                                            LEFT JOIN employee manager ON employee.manager_id = manager.id`);
+    console.table(employeeData);
     mainMenu();
 };
 const viewDepartments = async () => {
-    console.log('here');
     const [departmentData] = await db.query('SELECT * FROM department');
     console.table(departmentData);
+    mainMenu();
+};
+const viewRoles = async () => {
+    const [roleData] = await db.query(`SELECT role.id, role.title, role.salary, department.name AS departmnent
+                                        FROM role 
+                                        LEFT JOIN department ON role.department_id = department.id`);
+    console.table(roleData);
     mainMenu();
 };
 mainMenu();
